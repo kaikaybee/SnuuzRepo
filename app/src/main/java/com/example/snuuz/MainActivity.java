54e6e4f2ac476c31c082c59e49586a86e0aabcb2
Elijah Holmberg <eholmber@ucsc.edu>
2019-10-27 15:42:41 -0700

    Added Custom Toolbar with Functioning Buttons

--- app/src/main/java/com/example/snuuz/MainActivity.java
+++ app/src/main/java/com/example/snuuz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.snuuz;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_clock:
+                //call clock
+                return true;
+
+            case R.id.action_history:
+                //call history
+                return true;
+
+            case R.id.action_settings:
+                //call settings
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

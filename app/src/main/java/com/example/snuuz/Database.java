dee5964321c9cd9e034aba8050e6dc1ca303b8e1
christinacarlisle <chensonc@ucsc.edu>
2019-11-03 19:10:31 -0800

    database

---
+++ app/src/main/java/com/example/snuuz/Database.java
@@ -0,0 +1,46 @@
+package com.example.snuuz;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
+
+public class Database extends AppCompatActivity {
+    MyDB db;
+    EditText date;
+    EditText wake_up;
+    EditText sleep;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+        db = new MyDB(this, "Sleep_Tracker", null, 1);
+        date = findViewById(R.id.edit1);
+        wake_up = findViewById(R.id.edit2);
+        sleep = findViewById(R.id.edit3);
+    }
+
+    public void insert(View view) {
+        String s1 = date.getText().toString();
+        String s2 = wake_up.getText().toString();
+        String s3 = sleep.getText().toString();
+        db.insert(s1, s2, s3);
+    }
+
+    public void delete(View view) {
+        String s = date.getText().toString();
+        db.delete(s);
+    }
+
+    public void view(View view) {
+        db.getAll();
+    }
+
+    public void update(View view) {
+        String s1 = date.getText().toString();
+        String s2 = wake_up.getText().toString();
+        String s3 = sleep.getText().toString();
+        db.update(s1, s2, s3);
+    }
+}
---
+++ app/src/main/java/com/example/snuuz/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.snuuz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchDatabaseActivity(View view) {
+        Intent intent = new Intent(this, Database.class);
+        startActivity(intent);
+    }
+}

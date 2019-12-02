8230330e669f299732031bc393d5030a5e89d375
Brian <bbasaldu@ucsc.edu>
2019-12-01 16:28:28 -0800

    Merge Finished

--- SnuuzFiles/app/src/main/java/com/example/snuuz/MainActivity.java
+++ SnuuzFiles/app/src/main/java/com/example/snuuz/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.snuuz;
 
 import androidx.appcompat.app.AppCompatActivity;
-<<<<<<< .merge_file_lKBRKc
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
@@ -15,7 +15,7 @@ import java.util.Date;
 
 import android.app.TimePickerDialog;
 import android.app.TimePickerDialog.OnTimeSetListener;
-=======
+
 
 import android.os.Bundle;
 import java.util.Calendar;
@@ -27,14 +27,14 @@ import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
->>>>>>> .merge_file_jX7JLV
+
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-<<<<<<< .merge_file_lKBRKc
+
 import android.widget.Toast;
 
 import android.content.Intent;
@@ -87,33 +87,13 @@ public class MainActivity extends AppCompatActivity {
         textAlarmPrompt = findViewById(R.id.alarm_prompt);
         buttonStartSetDialog = findViewById(R.id.startSetDialog);
         buttonStartSetDialog.setOnClickListener(new OnClickListener() {
-=======
-public class MainActivity extends AppCompatActivity {
 
-    TimePicker myTimePicker;
-    Button buttonstartSetDialog;
-    Button buttonCancelAlarm;
-    TextView textAlarmPrompt;
-
-    TimePickerDialog timePickerDialog;
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        textAlarmPrompt = (TextView)findViewById(R.id.alarmprompt);
-
-        buttonstartSetDialog = (Button)findViewById(R.id.startSetDialog);
-        buttonstartSetDialog.setOnClickListener(new OnClickListener(){
-
->>>>>>> .merge_file_jX7JLV
             @Override
             public void onClick(View v) {
                 textAlarmPrompt.setText("");
                 openTimePickerDialog(false);
 
-<<<<<<< .merge_file_lKBRKc
+
             }
         });
         buttonCancelAlarm = findViewById(R.id.cancel);
@@ -157,22 +137,7 @@ public class MainActivity extends AppCompatActivity {
     //Brian's code - please comment
     //Uses default time picker dialog to let user set alarm.
     //Uses Calender to get current time and to set a new calender instance.
-=======
-            }});
 
-        buttonCancelAlarm = (Button)findViewById(R.id.cancel);
-        buttonCancelAlarm.setOnClickListener(new OnClickListener(){
-
-            @Override
-            public void onClick(View arg0) {
-                //cancelAlarm();
-            }});
-
-
-
-
-}
->>>>>>> .merge_file_jX7JLV
     private void openTimePickerDialog(boolean is24r){
         Calendar calendar = Calendar.getInstance();
 
@@ -186,16 +151,13 @@ public class MainActivity extends AppCompatActivity {
 
         timePickerDialog.show();
 
-<<<<<<< .merge_file_lKBRKc
+
 
     }
 
     //Brian's code - please comment
     //Takes inputted User alarm time and sets alarm
-=======
-    }
 
->>>>>>> .merge_file_jX7JLV
     OnTimeSetListener onTimeSetListener
             = new OnTimeSetListener(){
 
@@ -210,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
             calSet.set(Calendar.SECOND, 0);
             calSet.set(Calendar.MILLISECOND, 0);
 
-<<<<<<< .merge_file_lKBRKc
+
 //            if(calSet.compareTo(calNow) <= 0){
 //                //Today Set time passed, count to tomorrow
 //                calSet.add(Calendar.DATE, 1);
@@ -322,14 +284,5 @@ public class MainActivity extends AppCompatActivity {
         String s3 = sleep;
         db.update(s1, s2, s3);
     }
-=======
-            if(calSet.compareTo(calNow) <= 0){
-                //Today Set time passed, count to tomorrow
-                calSet.add(Calendar.DATE, 1);
-            }
 
-            //setAlarm(calSet);
-        }};
-
->>>>>>> .merge_file_jX7JLV
 }
